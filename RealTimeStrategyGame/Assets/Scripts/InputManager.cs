6d2211eb699b5c69d972c549ed944440c543cebf
JackSainsbury <JackSainsbury@hotmail.co.uk>
2018-08-10 17:06:29 +0100

    Basic camera controlls over terrain

---
+++ RealTimeStrategyGame/Assets/Scripts/InputManager.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputManager : MonoBehaviour {
+
+    public delegate void UPAxisPress();
+    public static event UPAxisPress OnUPAxisPress;
+
+    public delegate void DownAxisPress();
+    public static event DownAxisPress OnDownAxisPress;
+
+    public delegate void LeftAxisPress();
+    public static event LeftAxisPress OnLeftAxisPress;
+
+    public delegate void RightAxisPress();
+    public static event RightAxisPress OnRightAxisPress;
+
+
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKey(KeyCode.W))
+        {
+            if (OnUPAxisPress != null)
+                OnUPAxisPress();
+        }
+
+        if (Input.GetKey(KeyCode.S))
+        {
+            if (OnDownAxisPress != null)
+                OnDownAxisPress();
+        }
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            if (OnLeftAxisPress != null)
+                OnLeftAxisPress();
+        }
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            if (OnRightAxisPress != null)
+                OnRightAxisPress();
+        }
+    }
+}
---
+++ RealTimeStrategyGame/Assets/Scripts/UserControls.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UserControls : MonoBehaviour {
+
+    public float m_moveSpeed = 10;
+
+    private bool m_moveFlag = false;
+
+    private int m_hitLayer;
+
+	// Use this for initialization
+	void Start () {
+        m_hitLayer = 1 << LayerMask.NameToLayer("Terrain");
+    }
+
+    private void Update()
+    {
+        if(m_moveFlag)
+        {
+            RaycastHit hit;
+
+            if(Physics.Raycast(transform.position + new Vector3(0, 100, 0), Vector3.down, out hit, 200, m_hitLayer))
+            {
+                transform.position = hit.point;
+            }
+        }
+    }
+
+    void OnEnable()
+    {
+        InputManager.OnUPAxisPress += MoveFWD;
+        InputManager.OnDownAxisPress += MoveBKWD;
+        InputManager.OnLeftAxisPress += MoveLT;
+        InputManager.OnRightAxisPress += MoveRT;
+    }
+
+
+    void OnDisable()
+    {
+        InputManager.OnUPAxisPress -= MoveFWD;
+        InputManager.OnDownAxisPress -= MoveBKWD;
+        InputManager.OnLeftAxisPress -= MoveLT;
+        InputManager.OnRightAxisPress -= MoveRT;
+    }
+
+
+    void MoveFWD()
+    {
+        transform.position += transform.forward * Time.deltaTime * m_moveSpeed;
+
+        m_moveFlag = true;
+    }
+    void MoveBKWD()
+    {
+        transform.position += -transform.forward * Time.deltaTime * m_moveSpeed;
+
+        m_moveFlag = true;
+    }
+    void MoveRT()
+    {
+        transform.position += transform.right * Time.deltaTime * m_moveSpeed;
+
+        m_moveFlag = true;
+    }
+    void MoveLT()
+    {
+        transform.position += -transform.right * Time.deltaTime * m_moveSpeed;
+
+        m_moveFlag = true;
+    }
+}
